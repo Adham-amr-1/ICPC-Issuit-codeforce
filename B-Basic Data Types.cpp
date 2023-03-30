9b8cbd81418fe80447560d910ebab020b4217937
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-03-30 11:31:36 +0200

    Create B-Basic Data Types.cpp

---
+++ B-Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    long long l;
+    char c;
+    float f;
+    double d;
+    cin >> a >> l >> c >> f >> d;
+    cout << a << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d ;
+    return 0;
+}
